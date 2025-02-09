7c9b218243d29e44cf26f90c7711dea9577f6e7c
clecyo7 <clecyocrf@gmail.com>
2025-02-09 16:01:29 -0300

    add index.vue

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h1>Index Page</h1>
+      <p>Welcome to my application!</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Index',
+  }
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #42b983;
+  }
+  </style>
+  
